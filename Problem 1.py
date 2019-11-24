779001455da364f2fc0a1c6e78feef4baf97b38b
jowietoong <57376455+jowietoong@users.noreply.github.com>
2019-11-24 22:15:43 +0800

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np
 A = pd.read_csv('cars.csv')
-B = A.iloc[np.r_[0:5, 27:32]] 
-
+first = A.iloc[0:5]
+last = A.iloc[27:32] 
+B = [first,last]
+C = pd.concat(B)
